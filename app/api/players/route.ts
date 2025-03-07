0909d5903ae30bbb52d3f1263c0e3c2bc36193a6
Lafiasabigado <lafiasabigado97@gmail.com>
2025-03-07 17:32:38 +0100

    add api token

--- app/api/players/route.ts
+++ app/api/players/route.ts
@@ -1,26 +1,49 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  try {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-    const apiToken = process.env.API_TOKEN;
+async function fetchWithAuth(url: string) {
+  let accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
+  const refreshToken = process.env.NEXT_PUBLIC_REFRESH_TOKEN;
 
-    if (!apiToken) {
-      throw new Error("Le token API est manquant !");
-    }
+  let response = await fetch(url, {
+    headers: {
+      "Authorization": `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+    },
+  });
 
-    const response = await fetch(`${apiUrl}/player?limit=8`, {
-      headers: {
-        "Authorization": `Bearer ${apiToken}`,
-        "Content-Type": "application/json",
-      },
+  if (response.status === 401) {
+    console.log("🔄 Token expiré, tentative de rafraîchissement...");
+
+    const refreshResponse = await fetch("https://foot-api-0gcy.onrender.com/api/token/refresh/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ refresh: refreshToken }),
     });
 
-    if (!response.ok) {
-      throw new Error("Erreur lors de la récupération des joueurs");
-    }
+    if (refreshResponse.ok) {
+      const refreshData = await refreshResponse.json();
+      accessToken = refreshData.access;
 
-    const data = await response.json();
+      console.log("✅ Nouveau token récupéré :", accessToken);
+
+      // Refaire la requête avec le nouveau token
+      response = await fetch(url, {
+        headers: {
+          "Authorization": `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      });
+    } else {
+      console.log("❌ Échec du rafraîchissement du token.");
+    }
+  }
+
+  return response.json();
+}
+
+export async function GET() {
+  try {
+    const data = await fetchWithAuth("https://foot-api-0gcy.onrender.com/player?limit=8");
 
     const cleanedResults = data.results.map((player: any) => ({
       ...player,
